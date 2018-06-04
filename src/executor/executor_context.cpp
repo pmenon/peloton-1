0159ed339941d05f83a376c647404b4382ba8f2f
Prashanth Menon <pmenon@cs.cmu.edu>
2018-06-04 13:41:47 -0400

    Fixes

--- src/executor/executor_context.cpp
+++ src/executor/executor_context.cpp
@@ -60,31 +60,48 @@ ExecutorContext::ThreadStates::ThreadStates(type::EphemeralPool &pool)
     : pool_(pool), num_threads_(0), state_size_(0), states_(nullptr) {}
 
 void ExecutorContext::ThreadStates::Reset(const uint32_t state_size) {
+  // If we've previously allocated some state, free it up now
   if (states_ != nullptr) {
     pool_.Free(states_);
     states_ = nullptr;
   }
+
+  // Clear # threads
   num_threads_ = 0;
-  // Always fill out to nearest cache-line to prevent false sharing of states
-  // between different threads.
-  uint32_t pad = state_size & ~CACHELINE_SIZE;
+
+  // Always fill out the thread-local state space to nearest cache-line to
+  // prevent false sharing of states between different threads. This is because
+  // all thread states are allocated in contiguous space.
+
+  uint32_t pad = state_size & static_cast<uint32_t>((CACHELINE_SIZE - 1));
   state_size_ = state_size + (pad != 0 ? CACHELINE_SIZE - pad : pad);
 }
 
 void ExecutorContext::ThreadStates::Allocate(const uint32_t num_threads) {
-  PELOTON_ASSERT(state_size_ > 0);
-  PELOTON_ASSERT(states_ == nullptr);
+  // There better not be any pre-existing state that we're overwriting
+  PELOTON_ASSERT(states_ == nullptr && "State was not reset after use");
+
+  // Set threads
   num_threads_ = num_threads;
+
+  // Allocate contiguous state space for all threads
   uint32_t alloc_size = num_threads_ * state_size_;
   states_ = reinterpret_cast<char *>(pool_.Allocate(alloc_size));
+
+  // Clear it
   PELOTON_MEMSET(states_, 0, alloc_size);
 }
 
 char *ExecutorContext::ThreadStates::AccessThreadState(
     const uint32_t thread_id) const {
-  PELOTON_ASSERT(state_size_ > 0);
-  PELOTON_ASSERT(states_ != nullptr);
-  PELOTON_ASSERT(thread_id < num_threads_);
+  PELOTON_ASSERT(
+      states_ != nullptr &&
+      "No thread state configured yet! Did you forget to call Reset()?");
+
+  PELOTON_ASSERT(thread_id < num_threads_ &&
+                 "Accessing state for non-existent thread");
+
+  // Done
   return states_ + (thread_id * state_size_);
 }
 
--- src/include/executor/executor_context.h
+++ src/include/executor/executor_context.h
@@ -74,13 +74,16 @@ class ExecutorContext {
     /// Access the state for the thread with the given id
     char *AccessThreadState(uint32_t thread_id) const;
 
+    template <typename T>
+    T *AccessThreadStateAs(uint32_t thread_id) const;
+
     /// Return the number of threads registered in this state
     uint32_t NumThreads() const { return num_threads_; }
 
     /// Iterate over each thread's state, operating on the element at the given
     /// offset only.
-    template <typename T>
-    void ForEach(uint32_t element_offset, std::function<void(T *)> func) const;
+    template <typename T, typename F>
+    void ForEach(uint32_t element_offset, const F &func) const;
 
    private:
     type::EphemeralPool &pool_;
@@ -107,15 +110,28 @@ class ExecutorContext {
   ThreadStates thread_states_;
 };
 
-template <typename T>
-inline void ExecutorContext::ThreadStates::ForEach(
-    uint32_t element_offset, std::function<void(T *)> func) const {
-  PELOTON_ASSERT(element_offset < state_size_);
+////////////////////////////////////////////////////////////////////////////////
+///
+/// Implementation below
+///
+////////////////////////////////////////////////////////////////////////////////
+
+template <typename T, typename F>
+inline void ExecutorContext::ThreadStates::ForEach(uint32_t element_offset,
+                                                   const F &func) const {
+  PELOTON_ASSERT(element_offset < state_size_ &&
+                 "Accessing an element beyond valid offset in thread state");
   for (uint32_t tid = 0; tid < NumThreads(); tid++) {
-    auto *elem_state =
-        reinterpret_cast<T *>(AccessThreadState(tid) + element_offset);
+    auto *thread_state = AccessThreadState(tid);
+    auto *elem_state = reinterpret_cast<T *>(thread_state + element_offset);
     func(elem_state);
   }
+}
+
+template <typename T>
+T *ExecutorContext::ThreadStates::AccessThreadStateAs(
+    uint32_t thread_id) const {
+  return reinterpret_cast<T *>(AccessThreadState(thread_id));
 }
 
 }  // namespace executor
